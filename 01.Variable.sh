0fa516daee0f95f75c9574d8faaa9c55a73d93ae
PrasadDev9642 <prasadlanka9642@gmail.com>
2026-08-03 16:31:48 +0530

    first commit

---
+++ 01.Variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+name="Prasad"
+echo "Hello, my name is $name"
+
+
+# Create a Variable 
+Name=Prasad
+Age=28
+City=Machilipatnam
+
+echo " My Name is : ${Name} | My Age is : ${Age} | I am From : ${City} "
+
+

777445b2d87cc221e3c34b24d08f60a76b76e30e
PrasadDev1432 <prasadlanka1432@gmail.com>
2026-08-04 00:23:46 +0530

    newfile added

--- 09.Conditions.sh
+++ 09.Conditions.sh
@@ -11,8 +11,10 @@ num=$1
 
 if [ $num -gt 10 ]; then
     echo "Given Number $num is greater then 10"
+	exit 1
 elif [ $num -lt 10 ]; then
     echo "Given Number $num is less then 10"
+	exit 1
 else
 	echo "Given Number $num is equal to 10"
 

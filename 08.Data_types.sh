5b72e4c36996c0c314aa8fbfc063c1f5f174f710
PrasadDev1432 <prasadlanka1432@gmail.com>
2026-08-03 23:59:48 +0530

    newfile added

--- 08.Data_types.sh
+++ 08.Data_types.sh
@@ -14,6 +14,7 @@ echo $((num + 5))
 
 #3. Float/Decimal  --------> Shell doesn’t support floats directly. You need bc or awk.
 echo "10.5 + 2.3" | bc
+awk 'BEGIN {print 10.5 + 2.3}'
 
 #4. Boolean    --------> Represented using true or false (exit codes).
 if true; then
@@ -32,3 +33,5 @@ echo ${arr[@]}   # All elements
 #7. Environment Variables  --------> Special variables available system-wide.
 echo $HOME
 export COURSE="DevOps"
+
+echo $COURSE

8dc13f553256c1b9d5758558d036a8889dcb7e07
PrasadDev1432 <prasadlanka1432@gmail.com>
2026-08-03 23:54:32 +0530

    newfile added

---
+++ 08.Data_types.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# everything in shell is considerd as string
+
+#📌 Shell Script Data Types
+
+# 1. String ---->  Default type for all variables.
+name="Prasad"
+echo $name
+
+#2. Integer -------> Used for arithmetic operations.
+num=10
+echo $((num + 5))
+
+#3. Float/Decimal  --------> Shell doesn’t support floats directly. You need bc or awk.
+echo "10.5 + 2.3" | bc
+
+#4. Boolean    --------> Represented using true or false (exit codes).
+if true; then
+  echo "Yes"
+fi
+
+#5. Character  ------> Treated as a string of length 1.
+char="A"
+echo $char
+
+#6. Array  -----> Indexed collections of strings.
+arr=("Linux" "Shell" "Docker")
+echo ${arr[0]}   # Linux
+echo ${arr[@]}   # All elements
+
+#7. Environment Variables  --------> Special variables available system-wide.
+echo $HOME
+export COURSE="DevOps"

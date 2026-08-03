60932812f7b7fe71526e9d675572a528f7de8e66
PrasadDev1432 <prasadlanka1432@gmail.com>
2026-08-03 19:12:22 +0530

    newfile added

--- 03.Variables.sh
+++ 03.Variables.sh
@@ -4,16 +4,12 @@
 A=$1
 B=$2
 
+read COURSE
+read ID
+read -s Phn_Number 
+
+
 echo "$A:: Hi $2, How are you?"
 echo "$B:: Hello $A, I am fine. How are you?"
-echo "$A:: What are you learning recently? And Suggest on Cource 31123share ur phn number also"
-echo "$B:: I am learning $COURSE $A This is My Phn Number : 9642432443"
-
-
-# read ID
-# read -s Phn_Number 
-
-# echo "$A:: Hi $2, How are you?"
-# echo "$B:: Hello $A, I am fine. How are you?"
-# echo "$A:: What are you learning recently? And Suggest on Cource ID and share ur phn number also"
-# echo "$B:: I am learning $COURSE $A This is My Phn Number : Phn_Number
+echo "$A:: What are you learning recently? And Suggest on Cource $ID and share ur phn number also"
+echo "$B:: I am learning $COURSE $A This is My Phn Numer : $Phn_Number"

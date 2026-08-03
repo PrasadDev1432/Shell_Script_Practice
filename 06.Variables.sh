60c0ca0c58a10cec039a304b01cbfb7633bba3c5
PrasadDev1432 <prasadlanka1432@gmail.com>
2026-08-03 23:39:06 +0530

    newfile added

--- 06.Variables.sh
+++ 06.Variables.sh
@@ -8,11 +8,3 @@ CITY="Machilipatnam"
 
 
 echo " My Name Is : $NAME ; I am $AGE yrs old ; I am From $CITY"
-
-# User Inputs using colors
-
-R="\e[31m"
-N="\e[37m"
-
-
-echo "My Shirt Clolor : ${R} RedColor Shirt ${N}"

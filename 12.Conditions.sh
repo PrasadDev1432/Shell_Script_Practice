e091e5ad89cbb34536d2eafb3fd8e98393451745
PrasadDev1432 <prasadlanka1432@gmail.com>
2026-08-04 17:25:25 +0530

    New Code

--- 12.Conditions.sh
+++ 12.Conditions.sh
@@ -9,17 +9,18 @@ N="\e[0m"
 
 
 if [ $USERID -ne 0 ]; then
-    echo "$R ERROR:: Please run this script with root privelege $N "
+    echo " $R ERROR:: Please run this script with root privelege $N "
 	exit 1
-elif [ $USERID -eq 0 ]; then
-    dnf install mysql -y
-	echo " $G Installation MySQL is Success"
-	exit 1
-else 
-   echo "Status Success"
 fi
 
+dnf install mysql -y
 
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing MySQL is failure"
+    exit 1
+else
+    echo "Installing MySQL is SUCCESS"
+fi
 
 
 

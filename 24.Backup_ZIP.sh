13f11ec832ce41e359c85dfab96c9c20cf489302
PrasadDev1432 <prasadlanka1432@gmail.com>
2026-08-29 17:15:17 +0530

    instance

--- 24.Backup_ZIP.sh
+++ 24.Backup_ZIP.sh
@@ -1,29 +1,43 @@
 #!/usr/bin/bash
 
-R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
 Source_Dir="$PWD/Log_files"
 Dest_Dir="$PWD/Backup_Log_files"
 
-if [ -d "$Source_Dir" ]; then
-    echo -e " $R  ............................$Source_Dir does not exist $N"
-    exit 1
-else
+# Source directory
+if [ ! -d "$Source_Dir" ]; then
+    echo -e "${Y}$Source_Dir does not exist, creating...${N}"
     mkdir -p "$Source_Dir"
-fi
-
-if [ -d "$Dest_Dir" ]; then
-    echo -e " $R  ............................$Dest_Dir does not exist $N"
-    exit 1
 else
-    mkdir -p "$Dest_Dir"
+    echo -e "${G}$Source_Dir already exists${N}"
 fi
 
+# Destination directory
+if [ ! -d "$Dest_Dir" ]; then
+    echo -e "${Y}$Dest_Dir does not exist, creating...${N}"
+    mkdir -p "$Dest_Dir"
+else
+    echo -e "${G}$Dest_Dir already exists${N}"
+fi
 
-touch -d "20days ago" "$Source_Dir"/{app,mongodb,user,cart,catalogue,mysql,shipping,payment,dispatch,frontend,redis,rabbitmq}.log
+# Create 20 days old files
+touch -d "20 days ago" "$Source_Dir"/{app,mongodb,user,cart,catalogue,mysql,shipping,payment,dispatch,frontend,redis,rabbitmq}.log
 
+# Create 20 current date log files
 touch "$Source_Dir"/app{1..20}.log
 
-find "$Source_Dir" -type f -name "*.log" -mtime +19 | zip -j Source_zip.zip -@ && find "$Source_Dir" -type f -name "*.log" -mtime +19 -delete
+# Find old logs -> ZIP -> if ZIP success delete old logs
+find "$Source_Dir" -type f -name "*.log" -mtime +19 |
+zip -j "$Source_Dir/Source_zip.zip" -@ &&
+find "$Source_Dir" -type f -name "*.log" -mtime +19 -delete
 
+
+
+# Move ZIP file to Backup directory
 find "$Source_Dir" -type f -name "*.zip" -exec mv {} "$Dest_Dir/" \;
+
+
+
